570074266287392dcaa5bd54d586695d2eee7719
dey_gu <87346972+degu0@users.noreply.github.com>
2024-05-31 17:17:42 -0300

    Feed project

---
+++ src/components/Comment.tsx
@@ -0,0 +1,56 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+import { useState } from 'react';
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        onDeleteComment(content)
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        });
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar
+                hasBorder={false}
+                src={"https://github.com/degu0.png"}
+            />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Deyvid Gustavo</strong>
+                            <time title='23 de Maio às 16:28' dateTime="2024-05-23 16:28:00">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário' onClick={handleDeleteComment}>
+                            <Trash />
+                        </button>
+                    </header>
+
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
